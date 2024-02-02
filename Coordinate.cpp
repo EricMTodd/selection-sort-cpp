397f1924e4a8fcb55e71aea134538b61fb035efa
EricMTodd <eterictodd@gmail.com>
2024-02-01 20:06:06 -0700

    Working on it

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -28,6 +28,13 @@ unsigned int Coordinate::getY() {
 unsigned int Coordinate::getDistance(Coordinate* secondPoint) {
 //the shortest distance between two points is a straight line
 //return the distance between this point and the secondPoint
-   return 1;
+   int deltaX = static_cast<int>(secondPoint->getX() - static_cast<int>(x));
+   int deltaY = static_cast<int>(secondPoint->getY() - static_cast<int>(y));
+
+   unsigned int squaredDistance = static_cast<unsigned int>(deltaX * deltaX + deltaY * deltaY);
+
+   unsigned int distance = static_cast<unsigned int>(std::sqrt(squaredDistance));
+
+   return distance;
 }
 
--- CoordinateList.cpp
+++ CoordinateList.cpp
@@ -31,9 +31,16 @@ void CoordinateList::createCenterPoint() {
 //assign private variable center to this new object
    int sumX = 0;
    int sumY = 0;
-   for (auto i : allPoints) {
-      std::cout<<i<<std::endl;
+
+   for (const auto& point : allPoints) {
+      sumX += point->getX();
+      sumY += point->getY();
    }
+
+   unsigned int avgX = sumX / allPoints.size();
+   unsigned int avgY = sumY / allPoints.size();
+
+   center = new Coordinate(avgX, avgY);
 }
 
 unsigned int CoordinateList::getDistanceFromCenter(unsigned int index) {
@@ -47,7 +54,19 @@ unsigned int CoordinateList::getDistanceFromCenter(unsigned int index) {
 unsigned int CoordinateList::findNextClosest(unsigned int start) {
 //given start, find the index of the next closest coordinate point to center remaining in the list 
 //searching from start to the end of the vector
-   return start;
+   unsigned int closestIndex = start;
+   unsigned int minDistance = getDistanceFromCenter(start);
+
+   for (unsigned int i = start + 1; i < allPoints.size(); ++i) {
+      unsigned int distance = getDistanceFromCenter(i);
+
+      if (distance < minDistance) {
+         closestIndex = i;
+         minDistance = distance;
+      }
+   }
+
+   return closestIndex;
 }
 
 Coordinate* CoordinateList::getCenter() {
@@ -59,5 +78,10 @@ void CoordinateList::orderListFromCenter() {
 //initialize the center variable
 //put the closest point to the center in location 0 in the vector
 //continue to put the points in order one by one by finding the next closest and swapping with whatever is next
- 
+   createCenterPoint();
+   for (unsigned int i = 0; i < allPoints.size() - 1; ++i) {
+      unsigned int closestIndex = findNextClosest(i);
+
+      std::swap(allPoints[i], allPoints[closestIndex]);
+   }
 }
